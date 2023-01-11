517c32ba1cf68806b375bcc8f3984423771848b0
VladVelik <sosinvladik@yandex.ru>
2023-01-11 10:15:47 +0300

    Initial Commit

---
+++ URLSessionApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  URLSessionApp
+//
+//  Created by Sosin Vladislav on 11.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
